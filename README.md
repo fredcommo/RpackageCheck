37d79dcfb469445f31f3a40f5a9e52231d6e20f5
frederic commo <fredcommo@MacBook-Pro-de-frederic.local>
2015-04-21 12:39:14 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Donwload the Rcheck.R file
+### Or source it from Github:
+
+```r
+# Install devtools first if needed.
+library(devtools)
+SourceURL <- "https://raw.github.com/"
+source_url(SourceURL)
+```
---
+++ Rcheck.R
@@ -0,0 +1,83 @@
+# indentPattern <- "^(?:\\s{4})*\\s{1,3}\\S"
+
+.check <- function(fileName, pattern = "^(?:\\s{4})*\\s{1,3}\\S"){
+    
+    if(length(fileName) == 0){
+        cat("No file.\n")
+        return(NULL)
+    }
+
+    cat("Checking", fileName, "...\n")
+    foo <- readLines(fileName)
+    check <- sapply(1:length(foo), function(ii){
+        
+        out <- NULL
+        
+        if(nchar(foo[ii])>80){
+            cat("** Line", ii, "> 80 char.\n")
+            out <- TRUE
+        }
+        
+        if(grepl("[\t]", foo[ii]))
+            if(grepl(pattern, foo[ii])){
+                cat("** line", ii, "is indented with tab(s)\n")
+                out <- TRUE
+            }
+        
+        else if(grepl("^ ", foo[ii]))
+            if(grepl(pattern, foo[ii])){
+                cat("** line", ii, "not indented with a multiple of 4 spaces\n")
+                out <- TRUE
+            }
+        
+        return(out)
+    })
+    
+    check <- do.call(c, check)
+    if(is.null(check))
+        cat("No error detected\n")
+    
+    cat("\n")
+}
+
+runCheck <- function(packagePath){
+
+    cat("############################\n")    
+    cat("Checking DESCRIPTION file...\n")
+    cat("############################\n")    
+    path <- file.path(packagePath, "DESCRIPTION")
+    .check(path)
+    
+    cat("############################\n")    
+    cat("Checking NAMESPACE file...\n")
+    cat("############################\n")    
+    path <- file.path(packagePath, "NAMESPACE")
+    .check(path)
+    
+    cat("############################\n")    
+    cat("Checking R code files...\n")
+    cat("############################\n")    
+    path <- file.path(packagePath, "R")
+    lf <- list.files(path, full.names = TRUE)
+    for(f in lf)
+        .check(f)
+    
+    cat("############################\n")    
+    cat("Checking Rd files in man...\n")
+    cat("############################\n")    
+    path <- file.path(packagePath, "man")
+    lf <- list.files(path, full.names = TRUE)
+    for(f in lf)
+        .check(f)
+    
+    cat("############################\n")    
+    cat("Checking Rnw files in vignettes...\n")
+    cat("############################\n")
+    path <- file.path(packagePath, "vignettes")
+    f <- list.files(path, full.names = TRUE, pattern = ".Rnw")
+    .check(f)
+}
+
+packagePath <- "/Users/fredcommo/Documents/myProjects/myRpackages/rCGH"
+runCheck(packagePath)
+
